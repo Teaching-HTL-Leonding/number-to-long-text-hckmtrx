e5894d0d549a941a53a5d772dbac3897f99f0682
hckmtrx <96651154+hckmtrx@users.noreply.github.com>
2023-02-02 10:26:58 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿string input;
+bool quit;
+do
+{
+    Console.Write("Enter a number: ");
+    input = Console.ReadLine()!.ToLower();
+    quit = input == "q";
+    bool inputIsValid = int.TryParse(input, out int number) || quit;
+
+    if (!inputIsValid) { Console.WriteLine("Invalid input. Try again..."); }
+    else if (!quit) { Console.WriteLine($"DigitIntoLongText: {DigitIntoLongText(number)}"); }
+} while (!quit);
+
+string DigitIntoLongText(int number)
+{
+    return number switch
+    {
+        0 => "zero",
+        1 => "one",
+        2 => "two",
+        3 => "three",
+        4 => "four",
+        5 => "five",
+        6 => "six",
+        7 => "seven",
+        8 => "eigth",
+        9 => "nine",
+        _ => "not a digit"
+    };
+}
